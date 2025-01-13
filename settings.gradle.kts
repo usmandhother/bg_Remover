f29c29321a729f65d0dbc94912e23a2cd61b9588
Muhammad Usman <usmandhother12@gmail.com>
2025-01-13 11:47:11 +0500

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven {
+            url = uri("https://developer.huawei.com/repo/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        maven {
+            url = uri("https://developer.huawei.com/repo/")
+        }
+        mavenCentral()
+
+    }
+}
+
+rootProject.name = "bg_remover"
+include(":app")
+ 
